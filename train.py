43564f8da5103ef8e1fbea60da6fff85da43d7f6
PrismoSoft <112518152+prismosoft@users.noreply.github.com>
2022-09-20 16:48:54 -0400

    Create train.py

---
+++ train.py
@@ -0,0 +1,22 @@
+from lib.solover.trainer import Trainer
+from lib.config.config import Config
+# from lib.utils.config import cfg
+from lib.lsp import LSP
+from torch.backends import cudnn
+from lib.models.audio2feature import Audio2Feature
+
+
+def main(cfg):
+    cudnn.benchmark = True
+    cudnn.deterministic = False
+    cudnn.enabled = True
+    lsp = LSP(cfg)
+    # a2f = Audio2Feature(cfg)
+    trainer = Trainer(model=lsp, config=cfg)
+    trainer.fit()
+    
+if __name__ == "__main__":
+    path = './config/audio2feature.yaml'
+    cfg = Config.fromfile(path)
+    
+    main(cfg)
